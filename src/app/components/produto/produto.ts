f5f80b291a5e7f0d3544bc4770088f5edd0c7372
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-07-13 21:55:17 -0300

    feat: adiciona os produtos

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import {UpperCasePipe} from '@angular/common';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+//criar classe Produto com as propriedades produto,preco
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos =[
+  {nome: 'mouse' , preco: 29.99},
+  {nome: 'teclado' , preco: 49.99},
+  {nome: 'Monitor' , preco: 149.99}  
+  ]
+}
