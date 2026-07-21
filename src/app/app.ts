f73faa6ff385ca622ec397d940af34a9458c25c9
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-07-21 20:25:08 -0300

    feat: adicionar RouterOutlet e RouterLink

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//import { RouterOutlet } from '@angular/router'; //remove a importação, pois não é necessario no momento para este componente
+//import {Produto} from './components/produto/produto'; //importando a classe Produto do arquivo produto.ts do componente app
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Mercado aleatorio'
 }
