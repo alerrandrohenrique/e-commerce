b68a4058594fb45998d1cb5450f21e77ddf3a062
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-07-22 21:52:09 -0300

    feat: implementar autenticação e proteção de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,27 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-
-export const routes: Routes = [    
-    {        
-        path:'',
-        component: ListaProdutos,
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
+export const routes: Routes = [
+    {
+        path:'', //!router para raiz localhost:4200/
+        loadComponent: () =>
+            import('./features/home/home/home')
+        .then((m) => m.Home),
     },
     {
-       path:'Carrinho',
-       component: Carrinho, 
+        path:'produto', //!router para produtos localhost:4200/
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos')
+        .then((m) => m.ListaProdutos),
+    },
+    {
+         path:'carrinho', //!router para carrinho localhost:4200/
+         canActivate: [authGuard],
+        loadComponent: () =>
+            import('./features/carrinho/carrinho/carrinho')
+        .then((m) => m.Carrinho),
+    },
+    {
+         path:'**', //!router para qualquer outra rota que seja as rotas acima, redirecionando para a raiz localhost:4200/
+         redirectTo: '/',
     },
 ];
