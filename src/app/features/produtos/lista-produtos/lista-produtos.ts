48803ebdc007e1e91976d2e6e6098eaaabfa0705
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-07-24 18:43:00 -0300

    feat: carregar produtos da API e calcular totais do carrinho

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -5,6 +5,8 @@ import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
@@ -12,14 +14,15 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos { 
-  //lista de produtos de dados - array
-  produtos = signal([
-    {nome:'Processador Intel Core', preco: 7299.99},
-    {nome:'Celular Xiaomi', preco: 2499.99},
-    {nome:'Fone Gamer', preco: 499.99},
-    {nome:'Placa de video', preco: 2700.99},
-    {nome:'Processador Ryzen', preco: 1499.99}
-  ]);
+ //! signal
+  produtos = signal <{nome: string; preco: number}[]>([]);
+
+  carregando = signal (true);
+
+  produtoSelecionado = signal <string | null>(null);
+
+  carrinho = signal<{nome: string; preco: number}[]>([]);
+  
   //!funçao para exibir produtos selecionados pelo usuario console
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
@@ -49,8 +52,28 @@ export class ListaProdutos {
       {nome: 'Processador Ryzen', preco: 2800.99 }
     ]);
   }
-  constructor() {
-    //! metodo para monitorar alterações em tempo real usando metodo effect()
+    carregarProdutos(){
+    this.carregando.set(true);
+    this.http.get<{title: string; price: number}[]>
+    ('https://fakestoreapi.com/products').subscribe({
+      next: (dados) => {
+        const produtosFormatados = dados.map(p => ({
+          nome: p.title,
+          preco: p.price
+        }));
+        this.produtos.set(produtosFormatados);
+        this.carregando.set(false);
+      },
+      error: (error) => {
+        console.error('Erro ao carregar produtos: ', error);
+        this.carregando.set(false);
+      }
+    });
+  }
+  constructor (private http: HttpClient) {
+    //! carrega a API
+    this.carregarProdutos();
+    //! esffects continuam iguais
     effect(() =>{
       console.log('Lista de Produtos Alterados: ',this.produtos());
     });
@@ -63,10 +86,6 @@ export class ListaProdutos {
       }
      });
   } 
-  //! metodo para criar um estado de seleção com signal string | null
-  produtoSelecionado = signal <string | null>(null);
-  //! metodo para criar um estado para carrinho com signal
-  carrinho = signal <{nome: string; preco: number}[]> ([]);
   adicionarAoCarrinho (produto: {nome: string; preco: number}){
     this.carrinho.update(listaAtual => [
     ...listaAtual, produto]
@@ -82,3 +101,4 @@ export class ListaProdutos {
 
   )});
 }
+
