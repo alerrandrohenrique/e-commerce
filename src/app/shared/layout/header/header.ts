570b7efdd7c70369c55935c6949857681e82bf80
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-08-16 01:07:17 -0300

    refactor(header): integrar AuthService e navegação no logout

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { CarrinhoService } from '../../../core/Services/carrinho.service';
@@ -13,11 +13,12 @@ styleUrl: './header.css',
 export class Header {
 private carrinhoService = inject(CarrinhoService);
 private authService = inject(AuthService);
-quantidade = this.carrinhoService.QuantidadeItens
-;
+private router = inject(Router);
+quantidade = this.carrinhoService.QuantidadeItens;
 estaLogado = this.authService.estaLogado;
 usuarioAtual = this.authService.usuarioAtual;
 sair() {
 this.authService.logout();
+this.router.navigateByUrl('/login');
 }
 }
