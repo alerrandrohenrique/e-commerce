520bbbeeed358c9dd60af78844c7b1cea5363198
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-08-19 02:02:31 -0300

    feat(acesso-negado): criar lógica do componente de acesso restrito

--- src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
 selector: 'app-acesso-negado',
-imports: [RouterLink],
+imports : [RouterLink],
 templateUrl: './acesso-negado.html',
 styleUrl: './acesso-negado.css',
 })
