2f9b30fbe36595338f64f77c0474215660fbb647
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-08-19 20:00:32 -0300

    refactor(auth): reescrever authGuard utilizando AuthFacade

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,15 +1,11 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-
-import { AuthService } from './Services/auth.service';
-
+import { AuthFacade } from './Facades/auth.facade';
 export const authGuard: CanActivateFn = () => {
-const authService = inject(AuthService);
+const authFacade = inject(AuthFacade);
 const router = inject(Router);
-
-if (authService.estaLogado()) {
+if (authFacade.estaLogado()) {
 return true;
 }
-
 return router.createUrlTree(['/login']);
 };
